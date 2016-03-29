3a7f23357c98611b2b2ba4cf603def0f3f575b0a
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-03-29 16:07:46 +0200

    add and use install_eb_dep.sh script

---
+++ easybuild/scripts/install_eb_dep.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <name>-<version> <prefix>"
+    exit 1
+fi
+PKG=$1
+PREFIX=$2
+
+PKG_NAME=`echo $PKG | cut -f1 -d'-'`
+PKG_VERSION=`echo $PKG | cut -f2 -d'-'`
+
+if [ x$PKG_NAME == 'xmodules' ]; then
+    PKG_URL='http://prdownloads.sourceforge.net/modules/'
+    export PATH=$PREFIX/Modules/$PKG_VERSION/bin:PATH
+
+elif [ x$PKG_NAME == 'xlua' ]; then
+    PKG_URL='http://downloads.sourceforge.net/project/lmod/'
+    CONFIG_OPTIONS='--with-static=yes'
+    export PATH=$PREFIX/bin:$PATH
+
+elif [ x$PKG_NAME == 'xLmod' ]; then
+    PKG_URL='https://github.com/TACC/Lmod/archive/'
+    export PATH=$PREFIX/lmod/$PKG_VERSION/libexec:$PATH
+else
+    echo "ERROR: Unknown package name '$PKG_NAME'"
+    exit 2
+fi
+
+echo "Installing ${PKG} @ ${PREFIX}..."
+wget ${PKG_URL}/${PKG}.tar.gz
+tar xfz ${PKG}.tar.gz && cd ${PKG}
+./configure $CONFIG_OPTIONS --prefix=$PREFIX && make && make install
