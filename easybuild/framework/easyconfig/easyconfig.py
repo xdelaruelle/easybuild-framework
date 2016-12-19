3cd803955cb90529812f4b1fae0041baa8495133
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-12-19 09:26:23 +0100

    fix determining 'new' value in copy_easyconfigs

--- easybuild/framework/easyconfig/easyconfig.py
+++ easybuild/framework/easyconfig/easyconfig.py
@@ -1484,9 +1484,10 @@ def copy_easyconfigs(paths, target_dir):
 
             target_path = det_location_for(path, target_dir, soft_name, ec_filename)
 
+            file_info['new'].append(not os.path.exists(target_path))
+
             copy_file(path, target_path, force_in_dry_run=True)
             file_info['paths_in_repo'].append(target_path)
-            file_info['new'].append(os.path.exists(target_path))
 
         else:
             raise EasyBuildError("Multiple EasyConfig instances obtained from easyconfig file %s", path)
